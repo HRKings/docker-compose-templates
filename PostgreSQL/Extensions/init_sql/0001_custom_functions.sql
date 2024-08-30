14f84549d0490a555bdf664703bab2718a672e60
Helton Reis <hrkings@rhaizzing.com>
2024-08-30 00:43:26 -0300

    improvement: make all postgres helper functions safe and parallel

--- PostgreSQL/Extensions/init_sql/0001_custom_functions.sql
+++ PostgreSQL/Extensions/init_sql/0001_custom_functions.sql
@@ -2,14 +2,14 @@
 CREATE OR REPLACE FUNCTION get_ulid_from_timestamp_day(DATE)
 RETURNS ulid AS $$
   SELECT $1::timestamp::ulid
-$$ LANGUAGE SQL IMMUTABLE;
+$$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE;
 COMMENT ON FUNCTION get_ulid_from_timestamp_day IS 'This function returns a zeroed ULID generated only from the DATE part of the timestamp';
 
 CREATE OR REPLACE FUNCTION is_ulid_within_day(ulid, DATE)
 RETURNS bool AS $$
   SELECT $1 >= get_ulid_from_timestamp_day($2)
     AND $1 <= get_ulid_from_timestamp_day(($2 + INTERVAL '1 day')::date)
-$$ LANGUAGE SQL IMMUTABLE;
+$$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE;
 COMMENT ON FUNCTION is_ulid_within_day IS 'This function checks if a given ULID timestamp DATE part is the same of the provided timestamp';
 
 -- Easy filter of timestamps by one day
@@ -17,13 +17,13 @@ CREATE OR REPLACE FUNCTION is_timestamp_within_day(TIMESTAMP WITHOUT TIME ZONE, 
 RETURNS bool AS $$
   SELECT $1 >= $2
     AND $1 < ($2 + INTERVAL '1 day')
-$$ LANGUAGE SQL IMMUTABLE;
+$$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE;
 COMMENT ON FUNCTION is_timestamp_within_day(TIMESTAMP WITHOUT TIME ZONE, DATE) IS 'This function checks if a given timestamp DATE part is the same of the provided timestamp';
 
 CREATE OR REPLACE FUNCTION is_timestamp_within_day(TIMESTAMP WITH TIME ZONE, DATE)
 RETURNS bool AS $$
   SELECT $1 >= $2
     AND $1 < ($2 + INTERVAL '1 day')
-$$ LANGUAGE SQL IMMUTABLE;
+$$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE;
 COMMENT ON FUNCTION is_timestamp_within_day(TIMESTAMP WITH TIME ZONE, DATE) IS 'This function checks if a given timestamp DATE part is the same of the provided timestamp';
 --
