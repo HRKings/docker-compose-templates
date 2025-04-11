d79dc2e00cfca147b4cef0f4d118c5809433d26a
Helton Reis <hrkings@rhaizzing.com>
2025-04-10 22:45:50 -0300

    refactor(postgres/extensions): minor cleanup

--- PostgreSQL/Extensions/README.md
+++ PostgreSQL/Extensions/README.md
@@ -2,11 +2,11 @@
 
 This custom dockerfile contains some extensions that might be useful in production or development, also activates [pg_contrib_modules](https://www.postgresql.org/docs/current/contrib.html) and some custom functions
 
-# Current extensions
+## Current extensions
 
 - [ULID](https://github.com/HRKings/pgx_ulid): 128-bit sortable IDs compatible with the existing UUID. ([currently forked for compatibility with PG16](https://github.com/pksunkara/pgx_ulid))
 
-# Postgres Contrib Modules
+## Postgres Contrib Modules
 
 - [pg_stat_statements](https://www.postgresql.org/docs/current/pgstatstatements.html): view various statistics of the database
 - [pgrowlocks](https://www.postgresql.org/docs/current/pgrowlocks.html): show row locking information for a specified table
@@ -16,35 +16,40 @@ This custom dockerfile contains some extensions that might be useful in producti
 - [fuzzystrmatch](https://www.postgresql.org/docs/current/fuzzystrmatch.html): provides fuzzy string matching and distance based string search
 - [tsm_system_rows](https://www.postgresql.org/docs/current/tsm-system-rows.html): allows the `TABLESAMPLE` select clause to return a specific amount of rows
 
-# Custom functions
+## Custom functions
 
-## ULID date matching
+### ULID date matching
+
 A custom function that can match ULIDs based only on the date. For example, to match all ULIDs created on `2000-01-01` usually one would need to run this query:
 
 ```sql
-SELECT * FROM example_table 
+SELECT * FROM example_table
 WHERE ulid_column >= '2000-01-01'::timestamp::ulid AND ulid_column < '2000-01-02'::timestamp::ulid;
 ```
 
 With the custom function this becomes:
+
 ```sql
 SELECT * FROM example_table WHERE is_ulid_within_day(ulid_column, '2000-01-01'::date);
 ```
 
 A function to generate an ULID based only on a date is also provided:
+
 ```sql
 SELECT get_ulid_from_timestamp_day('2000-01-01'::date);
 ```
 
-## Check if a give timestamp is from a day
+### Check if a give timestamp is from a day
+
 A custom function that can match timestamps based only on the date. For example, to match all timestamps created on `2000-01-01` usually one would need to run this query:
 
 ```sql
-SELECT * FROM example_table 
+SELECT * FROM example_table
 WHERE timestamp_column >= '2000-01-01'::timestamp AND timestamp_column < '2000-01-02'::timestamp;
 ```
 
 With the custom function this becomes:
+
 ```sql
 SELECT * FROM example_table WHERE is_timestamp_within_day(timestamp_column, '2000-01-01'::date);
 ```
--- PostgreSQL/Extensions/init_sql/0001_custom_functions.sql
+++ PostgreSQL/Extensions/init_sql/0001_custom_functions.sql
@@ -1,60 +1,66 @@
 -- Easy filtering for ULIDs using dates
 CREATE OR REPLACE FUNCTION get_ulid_from_timestamp_day(DATE)
-RETURNS ulid AS $$
-  SELECT $1::timestamp::ulid
+RETURNS ulid AS
+$$
+	SELECT $1::timestamp::ulid
 $$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE;
 COMMENT ON FUNCTION get_ulid_from_timestamp_day IS 'This function returns a zeroed ULID generated only from the DATE part of the timestamp';
 
 CREATE OR REPLACE FUNCTION is_ulid_within_day(ulid, DATE)
-RETURNS bool AS $$
-  SELECT $1 >= get_ulid_from_timestamp_day($2)
-    AND $1 <= get_ulid_from_timestamp_day(($2 + INTERVAL '1 day')::DATE)
+RETURNS BOOLEAN AS
+$$
+	SELECT $1 >= get_ulid_from_timestamp_day($2)
+		AND $1 <= get_ulid_from_timestamp_day(($2 + INTERVAL '1 day')::DATE)
 $$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE;
 COMMENT ON FUNCTION is_ulid_within_day IS 'This function checks if a given ULID timestamp DATE part is the same of the provided timestamp';
 
 CREATE OR REPLACE FUNCTION is_ulid_within_interval(ulid, TIMESTAMP, INTERVAL)
-    RETURNS bool AS $$
-SELECT $1 >= $2::ulid
-    AND $1 <= ($2 + $3)::ulid
+RETURNS BOOLEAN AS
+$$
+	SELECT $1 >= $2::ulid
+		AND $1 <= ($2 + $3)::ulid
 $$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE;
 COMMENT ON FUNCTION is_ulid_within_interval(ulid, TIMESTAMP, INTERVAL) IS 'This function checks if a given ULID is within the interval starting on provided timestamp';
 
 CREATE OR REPLACE FUNCTION is_ulid_within_interval(ulid, TIMESTAMPTZ, INTERVAL)
-    RETURNS bool AS $$
-SELECT $1 >= $2::ulid
-    AND $1 <= ($2 + $3)::ulid
+RETURNS BOOLEAN AS
+$$
+	SELECT $1 >= $2::ulid
+		AND $1 <= ($2 + $3)::ulid
 $$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE;
 COMMENT ON FUNCTION is_ulid_within_interval(ulid, TIMESTAMPTZ, INTERVAL) IS 'This function checks if a given ULID is within the interval starting on provided timestamp';
 
 -- Easy filter of timestamps by one day
 CREATE OR REPLACE FUNCTION is_timestamp_within_day(TIMESTAMP WITHOUT TIME ZONE, DATE)
-RETURNS bool AS $$
-  SELECT $1 >= $2
-    AND $1 < ($2 + INTERVAL '1 day')
+RETURNS BOOLEAN AS
+$$
+	SELECT $1 >= $2
+		AND $1 < ($2 + INTERVAL '1 day')
 $$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE;
 COMMENT ON FUNCTION is_timestamp_within_day(TIMESTAMP WITHOUT TIME ZONE, DATE) IS 'This function checks if a given timestamp DATE part is the same of the provided timestamp';
 
 CREATE OR REPLACE FUNCTION is_timestamp_within_day(TIMESTAMP WITH TIME ZONE, DATE)
-RETURNS bool AS $$
-  SELECT $1 >= $2
-    AND $1 < ($2 + INTERVAL '1 day')
+RETURNS BOOLEAN AS
+$$
+	SELECT $1 >= $2
+		AND $1 < ($2 + INTERVAL '1 day')
 $$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE;
 COMMENT ON FUNCTION is_timestamp_within_day(TIMESTAMP WITH TIME ZONE, DATE) IS 'This function checks if a given timestamp DATE part is the same of the provided timestamp';
 
 -- Easy filter of timestamps by interval
 CREATE OR REPLACE FUNCTION is_timestamp_within_interval(TIMESTAMP WITHOUT TIME ZONE, TIMESTAMP WITHOUT TIME ZONE, INTERVAL)
-    RETURNS bool AS
+RETURNS BOOLEAN AS
 $$
-SELECT $1 >= $2
-    AND $1 < ($2 + $3);
+	SELECT $1 >= $2
+		AND $1 < ($2 + $3);
 $$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE;
 COMMENT ON FUNCTION is_timestamp_within_interval(TIMESTAMP WITHOUT TIME ZONE, TIMESTAMP WITHOUT TIME ZONE, INTERVAL) IS 'This function checks if a given timestamp is within the interval starting on provided timestamp';
 
 CREATE OR REPLACE FUNCTION is_timestamp_within_interval(TIMESTAMP WITH TIME ZONE, TIMESTAMP WITH TIME ZONE, INTERVAL)
-    RETURNS bool AS
+RETURNS BOOLEAN AS
 $$
-SELECT $1 >= $2
-    AND $1 < ($2 + $3);
+	SELECT $1 >= $2
+		AND $1 < ($2 + $3);
 $$ LANGUAGE SQL IMMUTABLE PARALLEL SAFE;
 COMMENT ON FUNCTION is_timestamp_within_interval(TIMESTAMP WITH TIME ZONE, TIMESTAMP WITH TIME ZONE, INTERVAL) IS 'This function checks if a given timestamp is within the interval starting on provided timestamp';
 
